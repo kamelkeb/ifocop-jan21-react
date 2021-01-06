ac38e65fbcb987c4e65a9d3b3539419b237407cc
berbiche <berbiche@gmail.com>
2021-01-06 11:31:49 +0100

    énoncé TP1

---
+++ note2.js
@@ -0,0 +1,22 @@
+/**
+ * Mise en place d'un état au sein d'un composant:
+ */
+// useState
+/**
+ * C'est une fonction à laquelle on passe la valeur de l'état, et qui nous rend
+ * un tableau de deux éléments: le premier sera une référence à la valeur de l'état
+ * le deuxième sera une fonction qui permet de modifier l'état.
+ *
+ * remarque: les fonctions dont le nom commence par "use", sont des fonctions
+ * spéciales qui viennent ajouter des fonctionnalité spécifiques. Elles ont
+ * des règles d'usage que l'on vera petit à petit.
+ * Ces fonctions sont appelés "hooks".
+ */
+
+/**
+ * Ennoncé TP1:
+ * 1- Donner un peu de style pour agencer les boutons.
+ * 2- Extraire du code de App.js, un composant, que l'on appelera Buttons,
+ * et qui contiendra les 4 boutons mais sans contenir le carré coloré.
+ * 3- Faire en sorte que les styles de ce composant soient mis dans un module CSS.
+ */
--- src/App.js
+++ src/App.js
@@ -4,37 +4,33 @@ import { ColoredSquare } from "./components/ColoredSquare";
 import { Header } from "./components/Header/Header";
 
 const App = () => {
-  const username = "Kelly";
-  const ilFaitBeau = Math.random() < 0.5 ? true : false;
-
-  const message = ilFaitBeau
-    ? "Quelle belle journée! Accessoire de la journée: Chapeau!"
-    : "Attention au vent! Accessoire de la journée: Parapluie!";
-
   const colors = [
     { color: "pink", id: "mon_app_squarerd_li1" },
     { color: "blue", id: "mon_app_squarerd_li2" },
     { color: "green", id: "mon_app_squarerd_li3" },
     { color: "red", id: "mon_app_squarerd_li4" },
   ];
+  const tableau = useState("Kelly");
+  const username = tableau[0];
+  const setUsername = tableau[1];
+
+  const [compteur, setCompteur] = useState(100);
+  const [visible, setVisible] = useState(true);
+
   return (
     <div className="App">
       <Header username={username}></Header>
       Ma première app
-      <ColoredSquare color="green" side="400px" message={message}>
-        <button style={{ width: "80%" }}>Enfant de ColoredSquare</button>
-        <ColoredSquare color="red"></ColoredSquare>
-      </ColoredSquare>
-      <ColoredSquare color="grey" side="300px">
-        <ul>
-          {colors.map((c) => (
-            <li key={c.id}>
-              <ColoredSquare color={c.color}></ColoredSquare>
-            </li>
-          ))}
-        </ul>
-      </ColoredSquare>
-      <ColoredSquare color="red"></ColoredSquare>
+      <div>
+        <button onClick={() => setCompteur(compteur + 10)}>Agrandir</button>
+        <button onClick={() => setCompteur(compteur - 10)}>Raptisser</button>
+        La taille est: {compteur}
+      </div>
+      <div>
+        <button onClick={() => setVisible(true)}>Rendre visible</button>
+        <button onClick={() => setVisible(false)}>Cacher</button>
+      </div>
+      {visible && <ColoredSquare color="red" side={compteur}></ColoredSquare>}
     </div>
   );
 };
