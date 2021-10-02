468a0eb5f3e6e915a79c33a10c0a744f2f586966
berbiche <berbiche@gmail.com>
2021-10-02 03:13:41 +0200

    Check

---
+++ note0.js
@@ -0,0 +1,23 @@
+//exo application:
+
+/*             const nomVariable = {...user}
+1- définir un objet 'user' avec trois champs:
+    - un premier champ 'age', contenant un nombre
+    - un deuxième champ 'prénom', contenant une string
+    - un troisième champ 'adresse', lui même est un objet qui contient deux champs: 
+            - le premier est 'numRue', contenant un nombre
+            - le deuxième est 'nomRue', contenant une string
+
+2- définir une variable 'user2' qui sera un alias de l'objet défini au point 1
+3- effectuer une copie superficielle de 'user' dans une nouvelle variable appelée 'userCopy'
+
+4- modifier le champ age de 'user' puis faire un cosole.log de 'user2.age' suivi
+d'un autre console.log de 'userCopy.age', que remarquez vous?
+
+5- modifier le numéro de rue de l'objet adresse de l'objet user, puis faire un console.log
+de user2.adresse.numRue
+suivi d'un autre console.log de userCopy.adresse.numRue
+Que remarquez vous?
+
+champ = propriété = attribut 
+*/
